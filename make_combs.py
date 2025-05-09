db08f63ff91192dc39ec70255bbec82c8e196bf7
ltalamanca <ltalamanca@ethz.ch>
2025-05-09 17:27:52 +0200

    IMD 1.9

--- make_combs.py
+++ make_combs.py
@@ -6,17 +6,20 @@ import numpy as np
 
 
 
+def iterative_add_N(dict_start, N_add, save=True,save_dir='./combinations/'):
+    tmp_d=copy.deepcopy(dict_start)
+    N_start=dict_start[1][-1]
+    i=0
+    while i<N_add:
+        print(i)
+        tmp_d=add_1(tmp_d)
+        if save:
+            NM=save_dir+'N_'+str(N_start+i+1)+'.pk'
+            with open(NM, 'wb') as handle:
+                pickle.dump(tmp_d, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        i+=1
 
-def add_1(combinantions_dictionary, ND=5):
-    N=combinantions_dictionary[1][-1]+1
-    new_cd={1:np.append(combinantions_dictionary[1],N)}
-    diff=2
-    while diff<ND:
-        new_part=np.vstack([combinantions_dictionary[diff-1].T,np.array([N]*len(combinantions_dictionary[diff-1]))])
-        new_in=np.hstack([combinantions_dictionary[diff].T,new_part]).T
-        new_cd.update({(diff):new_in})
-
-        diff+=1
+    return(tmp_d)
 
 
 

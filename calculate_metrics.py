943605d89b6dc4276a637622c991b81642ac430c
ltalamanca <ltalamanca@ethz.ch>
2025-05-15 12:32:35 +0200

    IMD 1.4.24

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -176,6 +176,7 @@ parser.add_argument('--dir_WAs')
 parser.add_argument('--max_diff')
 parser.add_argument('--max_dims')
 parser.add_argument('--timeit')
+parser.add_argument('--keep_ratios_constant')
 
 
 
@@ -190,14 +191,14 @@ differentiate= 3 if type(args.differentiate)==type(None) else int(args.different
 start= 50 if type(args.start)==type(None) else int(args.start)
 stop= 110 if type(args.stop)==type(None) else int(args.stop)
 step= 10 if type(args.step)==type(None) else int(args.step)
-
+keep_ratios_constant= False if type(args.keep_ratios_constant)==type(None) else args.keep_ratios_constant=='True' 
 timeit= True if type(args.timeit)==type(None) else args.timeit=='True'
 max_diff= 4 if type(args.max_diff)==type(None) else int(args.max_diff)
 max_dims= np.inf if type(args.max_dims)==type(None) else int(args.max_dims)
 
 
 
-dict_kwargs={'differentiate':differentiate, 'return_wa':True, 'timeit':timeit,
+dict_kwargs={'differentiate':differentiate, 'return_wa':True, 'timeit':timeit,'keep_ratios_constant': keep_ratios_constant,
              'start':start, 'stop':stop,  'step':step, 'dir_WAs':args.dir_WAs, 'dir_scramblers':args.dir_scramblers, 'max_diff': max_diff, 'max_dims':max_dims}
 
 

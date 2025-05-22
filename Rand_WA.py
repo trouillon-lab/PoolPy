307f31ab7619d0872ee64606cea6f9fcb2a4d592
ltalamanca <ltalamanca@ethz.ch>
2025-05-22 12:28:26 +0200

    IMD 1.8.16

--- Rand_WA.py
+++ Rand_WA.py
@@ -251,11 +251,10 @@ differentiate= 3 if type(args.differentiate)==type(None) else int(args.different
 start= 50 if type(args.start)==type(None) else int(args.start)
 stop= 110 if type(args.stop)==type(None) else int(args.stop)
 step= 10 if type(args.step)==type(None) else int(args.step)
-keep_ratios_constant= False if type(args.keep_ratios_constant)==type(None) else args.keep_ratios_constant=='True' 
 timeit= True if type(args.timeit)==type(None) else args.timeit=='True'
 max_diff= 4 if type(args.max_diff)==type(None) else int(args.max_diff)
 
 
 
-dict_kwargs={'differentiate':differentiate, 'return_wa':True, 'timeit':timeit,'keep_ratios_constant': keep_ratios_constant,
+dict_kwargs={'differentiate':differentiate, 'return_wa':True, 'timeit':timeit,
              'start':start, 'stop':stop,  'step':step, 'dir_WAs':args.dir_WAs, 'dir_scramblers':args.dir_scramblers, 'max_diff': max_diff,}

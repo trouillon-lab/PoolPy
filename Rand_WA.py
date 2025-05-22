dec96ef5f6df92cfbe09e4239a4d986faf524ea5
ltalamanca <ltalamanca@ethz.ch>
2025-05-22 12:12:02 +0200

    IMD 1.8.1

--- Rand_WA.py
+++ Rand_WA.py
@@ -11,3 +11,15 @@ import copy
 
 
 from Functions import *
+
+
+
+def get_max_C(n_compounds, max_compounds):
+    return int(n_compounds/2) if max_compounds==0 else max_compounds
+def get_min_C(n_compounds, MC):
+    return int(np.sqrt(n_compounds)) if int(np.sqrt(n_compounds))<MC else int(MC/2)
+
+def get_max_W(n_compounds):
+    return int(np.log2(n_compounds))
+def get_min_W(n_compounds):
+    return int(2*np.sqrt(n_compounds))

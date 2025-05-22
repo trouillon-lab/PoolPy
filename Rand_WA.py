f290b1f36610f6db7d90b7565bf4e41ddab7cc62
ltalamanca <ltalamanca@ethz.ch>
2025-05-22 14:17:18 +0200

    IMD 2.0.0

--- Rand_WA.py
+++ Rand_WA.py
@@ -177,9 +177,9 @@ def rand_sweep_diff(n_compounds, max_diff, dir_scramblers, Npath, **kwargs):
                 extra_exp=WA_rand.shape[1]+min_tests
                 #.append(['Random', min_tests, np.max(np.sum(WA_rand, axis=0)), WA_rand.shape[0], int(perc_check),  extra_exp,1+perc_check/100])
                 full_file_dir=os.path.join(dpath,'Random_diff_'+str(diff)+'_NS_'+
-                                               str(n_compounds)+'_NW_'+str(WA_rand.shape[0])+
+                                               str(n_compounds)+'_NW_'+str(WA_rand.shape[1])+
                                                '_MS_'+str(np.max(np.sum(WA_rand, axis=0)))+
-                                                '_PC_'+ str(int(perc_check)) +'_EE_'+str(np.round(extra_exp,2))+".txt")
+                                                '_PC_'+ str(int(perc_check)) +'_ME_'+str(np.round(min_tests,2))+".txt")
                 if not os.path.exists(dpath):
                     os.makedirs(dpath)
                 open(full_file_dir, 'a').close()
@@ -200,9 +200,9 @@ def rand_sweep_diff(n_compounds, max_diff, dir_scramblers, Npath, **kwargs):
                 extra_exp=WA_rand.shape[1]+min_tests
                 #.append(['Random', min_tests, np.max(np.sum(WA_rand, axis=0)), WA_rand.shape[0], int(perc_check),  extra_exp,1+perc_check/100])
                 full_file_dir=os.path.join(dpath,'Random_diff_'+str(diff)+'_NS_'+
-                                               str(n_compounds)+'_NW_'+str(WA_rand.shape[0])+
+                                               str(n_compounds)+'_NW_'+str(WA_rand.shape[1])+
                                                '_MS_'+str(np.max(np.sum(WA_rand, axis=0)))+
-                                                '_PC_'+ str(int(perc_check)) +'_EE_'+str(np.round(extra_exp,2))+".txt")
+                                                '_PC_'+ str(int(perc_check)) +'_ME_'+str(np.round(min_tests,2))+".txt")
                 if not os.path.exists(dpath):
                     os.makedirs(dpath)
                 open(full_file_dir, 'a').close()

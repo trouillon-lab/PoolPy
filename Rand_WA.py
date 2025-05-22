425484ed7c9b78a3d2599519ee77fa4139c483e1
ltalamanca <ltalamanca@ethz.ch>
2025-05-22 12:27:27 +0200

    IMD 1.8.13

--- Rand_WA.py
+++ Rand_WA.py
@@ -244,3 +244,20 @@ parser.add_argument('--timeit')
 
 
 args = parser.parse_args()
+
+
+
+
+differentiate= 3 if type(args.differentiate)==type(None) else int(args.differentiate)
+start= 50 if type(args.start)==type(None) else int(args.start)
+stop= 110 if type(args.stop)==type(None) else int(args.stop)
+step= 10 if type(args.step)==type(None) else int(args.step)
+keep_ratios_constant= False if type(args.keep_ratios_constant)==type(None) else args.keep_ratios_constant=='True' 
+timeit= True if type(args.timeit)==type(None) else args.timeit=='True'
+max_diff= 4 if type(args.max_diff)==type(None) else int(args.max_diff)
+max_dims= np.inf if type(args.max_dims)==type(None) else int(args.max_dims)
+
+
+
+dict_kwargs={'differentiate':differentiate, 'return_wa':True, 'timeit':timeit,'keep_ratios_constant': keep_ratios_constant,
+             'start':start, 'stop':stop,  'step':step, 'dir_WAs':args.dir_WAs, 'dir_scramblers':args.dir_scramblers, 'max_diff': max_diff, 'max_dims':max_dims}

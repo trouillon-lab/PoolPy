b2ba8d77da001a47e396edad1514f3b7835270a4
ltalamanca <ltalamanca@ethz.ch>
2025-05-22 12:12:52 +0200

    IMD 1.8.3

--- Rand_WA.py
+++ Rand_WA.py
@@ -73,3 +73,55 @@ def find_rand_params_precomp(n_compounds:int, n_compounds_per_well=0, n_wells=0,
             break
 
     return Comp, Wells, min_tests, min_wa, min_pcheck
+
+
+def assign_wells_random_precomp(n_compounds:int,  differentiate:int,scrambler:dict, n_compounds_per_well=0, 
+                        n_wells=0, guesses=0, Evaluate=False, return_me=False, **kwargs)->np.array:
+    if guesses==0:
+        guesses=n_compounds
+    min_tests=np.inf
+
+    if n_compounds_per_well==0 or n_wells==0:
+        _,_, min_tests, WA_rand, p_check=find_rand_params_precomp(**kwargs)
+        if return_me:
+            return WA_rand,  min_tests, p_check
+        
+        return WA_rand
+        
+
+
+    if Evaluate:
+        second_axis=np.tile(np.arange(n_wells),n_compounds_per_well).reshape(n_compounds_per_well,-1)
+        for i in range(guesses):
+            idt=np.random.randint(0,n_compounds,size=(n_compounds_per_well,n_wells) )
+            well_assigner=np.zeros((n_compounds,n_wells))==1
+            well_assigner[idt, second_axis]=True
+            if guesses==1:
+                if return_me:
+                    mean_exp, _, _, p_check= mean_metrics_precomp(well_assigner=well_assigner, 
+                                                                differentiate=differentiate,scrambler=scrambler,**kwargs)
+                    return well_assigner, mean_exp, p_check
+                return well_assigner
+            mean_exp, _, _, p_check= mean_metrics_precomp(well_assigner=well_assigner,
+                                                        differentiate=differentiate, scrambler=scrambler, **kwargs)
+            if p_check<1:
+                if return_me:
+                    return well_assigner,  mean_exp, p_check
+                return well_assigner
+            elif mean_exp<min_tests: 
+                best_wa=well_assigner.copy()
+                min_tests=mean_exp
+                min_pcheck=p_check
+
+        if return_me:
+            return best_wa,  min_tests, min_pcheck
+        
+        return best_wa
+
+    _,_, min_tests, WA_rand, p_check=find_rand_params_precomp(n_compounds=n_compounds, differentiate=differentiate, 
+                                 n_compounds_per_well=n_compounds_per_well, n_wells=n_wells, 
+                                 guesses=guesses,scrambler=scrambler, **kwargs)
+    if return_me:
+        return WA_rand,  min_tests, p_check
+    
+    return WA_rand

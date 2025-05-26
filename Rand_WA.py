c827e7f7fd26791b1b838aca0e27a1e6e32d400a
ltalamanca <ltalamanca@ethz.ch>
2025-05-26 15:48:18 +0200

    IMD 2.1.19

--- Rand_WA.py
+++ Rand_WA.py
@@ -197,8 +197,7 @@ def rand_sweep_diff(n_compounds, max_diff, dir_scramblers, Npath, **kwargs):
                     os.makedirs(dpath)
                 if kwargs['one_liner']:
                     open(full_file_dir, 'a').close()
-
-
+                    
                 if not os.path.exists(WApath):
                     os.makedirs(WApath)
                 thisfile=os.path.join(WApath,'WA_Random_N_'+str(n_compounds)+'_diff_'+str(diff)+

0fb224d87d1e9ffd1103ce8b46cd7040e3beb9d2
ltalamanca <ltalamanca@ethz.ch>
2025-06-11 12:04:46 +0200

    IMD 2.4.8

--- Rand_WA.py
+++ Rand_WA.py
@@ -357,7 +357,7 @@ parser.add_argument('--n_wells')
 parser.add_argument('--rand_guesses')
 parser.add_argument('--max_redundancy')
 parser.add_argument('--min_redundancy')
-
+parser.add_argument('--overwrite')
 
 
 
@@ -380,13 +380,14 @@ max_redundancy= 4 if type(args.max_redundancy)==type(None) else int(args.max_red
 min_redundancy= 0.1 if type(args.min_redundancy)==type(None) else int(args.min_redundancy)
 one_liner= False if type(args.one_liner)==type(None) else args.one_liner=='True'
 cleanup= False if type(args.cleanup)==type(None) else args.cleanup
+overwrite= True if type(args.overwrite)==type(None) else args.overwrite=='True'
 
 
 
 dict_kwargs={'differentiate':differentiate, 'return_wa':True, 'timeit':timeit,'guesses':rand_guesses,
              'start':start, 'stop':stop,  'step':step, 'dir_WAs':args.dir_WAs, 'dir_scramblers':args.dir_scramblers, 
              'max_diff': max_diff,'max_redundancy':max_redundancy,'min_redundancy':min_redundancy,
-             'one_liner':one_liner, 'cleanup':cleanup}
+             'one_liner':one_liner, 'cleanup':cleanup, 'overwrite':overwrite}
 
 if type(args.max_compounds)!=type(None): 
     dict_kwargs.update({'max_compounds':int(args.max_compounds)})

41a3145d21d3076d5bba36bb7bd92d28c6786d30
ltalamanca <ltalamanca@ethz.ch>
2025-07-03 12:05:54 +0200

    IMD 2.9.6

--- pooling.py
+++ pooling.py
@@ -288,7 +288,9 @@ def server(input, output, session):
             output.summary_table.set(metrics_data)
 
             #TBLS=CR[1]
-            #DFFS={}
+            DFFS={}
+            table_path=os.path.join(diff_folder_path,'WAs')
+            TBLS=load_wa_matrices(table_path)
             for idx in TBLS.keys():
                 b1=TBLS[idx]
                 tmp1=pd.DataFrame(b1, columns=['Pool '+ str(i) for i in range(b1.shape[1])], index=['Sample '+ str(i) for i in range(b1.shape[0])])

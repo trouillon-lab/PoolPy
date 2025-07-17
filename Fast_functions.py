2e8735d071925d67a5730462a04d763ece4c6c59
ltalamanca <ltalamanca@ethz.ch>
2025-07-17 11:25:09 +0200

    IMD 2.10.11

--- Fast_functions.py
+++ Fast_functions.py
@@ -229,9 +229,9 @@ def get_max_C(n_compounds, max_compounds):
 def get_min_C(n_compounds, MC):
     return int(np.sqrt(n_compounds)) if int(np.sqrt(n_compounds))<MC else int(MC/2)
 
-def get_max_W(n_compounds):
-    return int(np.log2(n_compounds))
 def get_min_W(n_compounds):
+    return int(np.log2(n_compounds))
+def get_max_W(n_compounds):
     return int(2*np.sqrt(n_compounds))
 
 

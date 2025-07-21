142f92f71fca50bf613478c0877338c5ea779938
ltalamanca <ltalamanca@ethz.ch>
2025-07-21 10:47:26 +0200

    IMD 2.10.12

--- pool_N.py
+++ pool_N.py
@@ -13,7 +13,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Parse some arguments')
 parser.add_argument('--differentiate', type=int, default=2, help='An integer argument with default 2')
-parser.add_argument('--n_compounds', type=int, default=50, help='An integer argument with default 50')
+parser.add_argument('--n_samp', type=int, default=50, help='An integer argument with default 50')
 parser.add_argument('--guesses', type=int, default=5, help='An integer argument for guesses of random WA with default 20')
 parser.add_argument('--method', type=str, default='all', help="A string argument with default 'all'")
 parser.add_argument('--path', type=str, default='./pooling_results', help="A string argument with default './pooling_results'")
@@ -23,7 +23,7 @@ args = parser.parse_args()
 args_dict = vars(args)
 
 diff= args_dict['differentiate']
-n_compounds=args_dict['n_compounds']
+n_compounds=args_dict['n_samp']
 method=args_dict['method']
 this_path=args_dict['path']
 args_dict['save_dir']=copy.deepcopy(args_dict['path'])

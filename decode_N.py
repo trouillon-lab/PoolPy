4f0a103bc81010ce6452543842865420da3e6385
ltalamanca <ltalamanca@ethz.ch>
2025-07-22 11:57:35 +0200

    IMD 2.10.30

--- decode_N.py
+++ decode_N.py
@@ -18,10 +18,31 @@ from Fast_functions import *
 
 parser = argparse.ArgumentParser(description='Parse some arguments')
 parser.add_argument('--differentiate', type=int, default=2, help='An integer argument with default 2')
-parser.add_argument('--path_to_WA', type=str, default='./pooling_results', help="A string argument with default './pooling_results'")
+parser.add_argument('--path_to_WA', type=str, help="A string argument with default './pooling_results'")
 
 args = parser.parse_args()
 
 args_dict = vars(args)
+
+dira=args_dict['path_to_WA']
+diff=args_dict['differentiate']
+WA_df=pd.read_csv(dira, index_col=0)
+
+WA=WA_df.values
+n_compounds=WA.shape[1]
+scrambler={1:np.arange(n_compounds)}
+for j in range(2,diff+1):
+    scrambler.update({j:np.array(list(itertools.combinations(np.arange(n_compounds),j)))})
+WA_list=[]
+multi=[]
+
+scrambler={1:np.arange(n_compounds)}
+for j in range(2,diff+1):
+    scrambler.update({j:np.array(list(itertools.combinations(np.arange(n_compounds),j)))})
+WA_list=[]
+multi=[]
+
+decoded=decode_precomp(well_assigner=WA,differentiate= diff, scrambler=scrambler, 
+               readout=np.array((WA[0]+WA[5]).astype(bool).astype(int)))
 
 

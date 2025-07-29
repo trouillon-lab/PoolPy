f7ef4f5833daf02388be1845e51fb7b2460eb7cf
ltalamanca <ltalamanca@ethz.ch>
2025-07-29 10:35:15 +0200

    IMD 2.11.15

--- Functions.py
+++ Functions.py
@@ -304,6 +304,23 @@ def assign_wells_chinese(n_compounds:int,  differentiate:int, backtrack=False, s
                     WA[k,j]=True if ls_nc3[j][i]==ls_nc3[j][ii] else False
                 k+=1
         return(WA.T)
+    
+    if special_diff and differentiate==3:
+        q=np.ceil(np.log(n_compounds)/np.log(3))
+        t=int((q+5)*q/2)
+        WA=np.zeros((t, n_compounds))==1
+        ls_nc3=[list(i)[::-1] for i in [int_to_base(j,3).zfill(q) for j in range(n_compounds)]]
+        for i in range(q):
+            for ii in range(3):
+                for j in range(n_compounds):
+                    WA[3*i+ii,j]=True if ls_nc3[j][i]==ii else False
+        k=3*q
+        for i in range(q):
+            for ii in range(i+1,q):
+                for j in range(n_compounds):
+                    WA[k,j]=True if ls_nc3[j][i]==ls_nc3[j][ii] else False
+                k+=1
+        return(WA.T)
 
 
 
